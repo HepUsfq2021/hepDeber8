712f5f0180e67d2fa06c5f6fb719821da2ad27d8
Pablo Ruales <pablomartinruales@gmail.com>
2021-05-09 19:41:12 -0500

    uploading 'EDAnalyzer' folder with attempt at extractig information using stwunsch's 'AOD2NanoAODOutreachTool'... files compile without error, but no output is given

---
+++ LED_search/LEDAnalysis/ledanalysis_cfg.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+import FWCore.Utilities.FileUtils as FileUtils
+
+process = cms.Process("led")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+files = FileUtils.loadListFromFile("data/CMS_Run2011A_DoubleElectron_AOD_12Oct2013-v1_20000_file_index.txt")
+files.extend(FileUtils.loadListFromFile("data/CMS_Run2011A_DoubleElectron_AOD_12Oct2013-v1_20001_file_index.txt"))
+files.extend(FileUtils.loadListFromFile("data/CMS_Run2011B_DoubleElectron_AOD_12Oct2013-v1_00000_file_index.txt"))
+files.extend(FileUtils.loadListFromFile("data/CMS_Run2011B_DoubleElectron_AOD_12Oct2013-v1_20000_file_index.txt")) 
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+       *files
+    )
+)
+
+process.led = cms.EDAnalyzer('LEDAnalysis',InputCollection = cms.InputTag("gsfElectrons")
+)
+
+
+process.p = cms.Path(process.led)
